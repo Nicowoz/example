d8f44e7c4d43b7007ac79d9ce9f222bc0d3bad86
Nicowoz <nicol@LAPTOP-P2CCB7VP.>
2025-08-27 12:11:16 +0200

    ci setup

--- mycosmo/cosmology.py
+++ mycosmo/cosmology.py
@@ -1,8 +1,30 @@
+"""Cosmology.
+
+This module implements various cosmology routines.
+
+"""
+
+"""
+Parameters
+----------
+redshift : float or numpy.ndarray
+Redshift(s) at which the Hubble parameter should be calculated
+cosmo_dict : dict
+Dictionary of cosmological constants. 
+"""
+
 import numpy as np
 
-from .constants import Mpc, G
+from .constants import G, Mpc
+
 
 def hubble(redshift, cosmo_dict):
+    """Hubble Parameter.
+
+    Calculate the Hubble parameter at a given redshift using the cosmological parameter values provided.
+
+    """
+
     hubble_const = cosmo_dict["H0"]
     matter = cosmo_dict["omega_m_0"] * (1 + redshift) ** 3
     curvature = cosmo_dict["omega_k_0"] * (1 + redshift) ** 2
@@ -10,8 +32,8 @@ def hubble(redshift, cosmo_dict):
 
     return np.sqrt(hubble_const**2 * (matter + curvature + dark_energy))
 
+
 def critical_density(redshift, cosmo_dict):
     H_z_si = hubble(redshift, cosmo_dict) * 1e3 / Mpc
 
     return (3.0 * H_z_si**2) / (8.0 * np.pi * G)
-
